8ed4cb4b1d523a0e5a676e502a6a2b3aca27f604
n0rdy <shumyn.kolya@gmail.com>
2023-08-07 21:56:37 +0200

    Fixed logs noise for permissions issues case

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"n0rdy.me/remindme/cmd"
 	"n0rdy.me/remindme/common"
 	"n0rdy.me/remindme/logger"
@@ -10,9 +9,7 @@ import (
 
 func main() {
 	err := logger.SetupLogger(utils.GetOsSpecificLogsDir(), common.ClientLogsFileName)
-	if err != nil {
-		fmt.Println("setting up logger failed", err)
-	} else {
+	if err == nil {
 		defer logger.Close()
 	}
 
