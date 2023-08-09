d55299ce0758ddfdfa1ab6fbbce971e7e13a2c61
n0rdy <shumyn.kolya@gmail.com>
2023-08-09 02:09:25 +0200

    Fixed admin server command logs

--- cmd/adminServer.go
+++ cmd/adminServer.go
@@ -7,19 +7,19 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// adminStartServerCmd represents the adminStartServer command
-var adminStartServerCmd = &cobra.Command{
+// adminServerCmd represents the adminStartServer command
+var adminServerCmd = &cobra.Command{
 	Use:   "server",
 	Short: "To be run by the app to start the server: please, don't run it on your own, it might crash the app",
 	Long: `WARNING: To be run by the app to start the server: please, don't run it on your own, it might crash the app.
 
 Use "start" command instead if you need to start the remindme app.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		logger.Info("adminStartServer command: called")
+		logger.Info("adminServerCmd command: called")
 		httpserver.Start()
 	},
 }
 
 func init() {
-	adminCmd.AddCommand(adminStartServerCmd)
+	adminCmd.AddCommand(adminServerCmd)
 }
